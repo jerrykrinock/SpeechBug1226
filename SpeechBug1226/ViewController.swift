054cc7020dd043e61dda6874ef0517860ff5714b
Jerry Krinock <jerry@sheepsystems.com>
2016-12-26 14:11:52 -0800

    Initial Commit

---
+++ SpeechBug1226/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpeechBug1226
+//
+//  Created by Jerry on 16/12/26.
+//  Copyright © 2016 Jerry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
